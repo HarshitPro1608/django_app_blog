40bc457b6f3198f6df991e7fa34c034e06444c5c
Kartik-Agra-wal <f20200300@pilani.bits-pilani.ac.in>
2024-01-21 14:35:30 +0530

    path_fix

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -288,8 +288,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'blog/static/blog/')
+STATIC_URL = '/static/blog/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'django_project/static/blog')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
